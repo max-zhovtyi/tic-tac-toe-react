e68c0cdeac6ceafa5324c456d11a4a0ec58b6055
Max Zhovtyi <maximzov@hotmail.com>
2024-10-11 20:37:31 +0200

    Derive active player based on state

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,37 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
-  const [gameTurns, setGameTurns] = useState([]);  
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-
-    setActivePlayer((prevActivePlayer) => {
-      if (prevActivePlayer === "X") {
-        return "O";
-      } else {
-        return "X";
-      }
-    });
-
-    setGameTurns((prevTurns) => { 
+    setGameTurns((prevTurns) => {
       // Find out who should be the current player now. We can't use "activePlayer" as we can not rely on that value.
       // OBS! No merging two states.
-      let currentPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      let currentPlayer = deriveActivePlayer(prevTurns);
 
       const turn = {
         player: currentPlayer,
         square: {
           row: rowIndex,
-          col: colIndex
-        }
+          col: colIndex,
+        },
       };
 
-      const updatedTurns = [ turn, ...prevTurns];
+      const updatedTurns = [turn, ...prevTurns];
       // console.log(updatedTurns);
-      
+
       return updatedTurns;
     });
   }
@@ -44,17 +42,14 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        activePlayer: {activePlayer}
+        currentPlayer: {activePlayer}
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" isActive={activePlayer === 'X'} symbol="X" />
-          <Player initialName="Player 2" isActive={activePlayer === 'O'} symbol="O" />
+          <Player initialName="Player 1" isActive={activePlayer === "X"} symbol="X" />
+          <Player initialName="Player 2" isActive={activePlayer === "O"} symbol="O" />
         </ol>
-        <GameBoard 
-          onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      
+
       <Log turns={gameTurns} />
     </main>
   );
